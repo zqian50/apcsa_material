3abc849c5cd5440509880fb99255fefc35aeb44e
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-10-02 01:21:18 -0400

    Create MyStringMethods.java

---
+++ classwork/string_methods/MyStringMethods.java
@@ -0,0 +1,24 @@
+public class MyStringMethods{
+
+  // Explain how IndexOf method works in a few sentences
+  // Then implement your own myIndexOf
+  public static int myIndexOf(String myString, String targetString){
+    return 0;
+  }                             
+
+  // Explain how CompareTo method works in a few sentences/
+  // Then implement your own myCompareTo
+  public static int myCompareTo(String myString, String otherString){
+    return 0;
+  }
+
+  // Explain how parseInt method works in a few sentences/
+  // Then implement your own myParseInt()
+ public static int myparseInt(String myString){
+
+ }
+
+
+
+
+
