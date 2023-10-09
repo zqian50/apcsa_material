4625c4c2b49caa4e4cee70616089967f1c50f109
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-10-09 10:13:57 -0400

    Update ChessBoard.java

--- classwork/2D_arrays/chessboard/ChessBoard.java
+++ classwork/2D_arrays/chessboard/ChessBoard.java
@@ -14,6 +14,13 @@ public class ChessBoard
     //Do not make alterations to this method!
     public static void print(String[][] array)
     {
-
+        for(String[] row: array)
+        {
+            for(String thing: row)
+            {
+                System.out.print(thing + "\t");
+            }
+            System.out.println();
+        }
     }
 }
