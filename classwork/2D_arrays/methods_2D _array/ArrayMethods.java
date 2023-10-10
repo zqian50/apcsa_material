fb2a388edf4bbd2973fb2814bf5defc5d00119d0
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-10-10 00:43:55 -0400

    Create ArrayMethods.java

---
+++ classwork/2D_arrays/methods_2D _array/ArrayMethods.java
@@ -0,0 +1,22 @@
+public class ArrayMethods
+{
+    public static void main(String[] args)
+    {
+      // Create test cases here
+    }
+    
+    
+    public static void print(int[][] array){
+    
+    }
+  
+    public static int findSum(int[][] array){
+    
+    }
+        
+
+    public static int[][] transpose(int[][] array){
+    
+    } 
+  
+}
