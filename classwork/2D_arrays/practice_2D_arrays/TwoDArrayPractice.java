7458c2a7a1271a365d5609922b3106e9073dba60
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-10-10 23:38:41 -0400

    Create TwoDArrayPractice.java

---
+++ classwork/2D_arrays/practice_2D_arrays/TwoDArrayPractice.java
@@ -0,0 +1,56 @@
+public class TwoDArrayPractice
+{
+
+  public static void main(String[] args)
+    {
+      // Create test cases here
+    }
+
+  
+    /*
+     * Swaps all values in the specified 2 rows of array.
+    */
+  public static void rowSwap(int[][] array, int rowAIndex, int rowBIndex)
+    {
+        // Yor code here
+    }
+    
+    /**
+     * Swaps all values in the specified 2 columns of mat.
+     */
+    public static void colSwap(int[][] array, int colAIndex, int colBIndex)
+    {
+        // Your code her
+    }
+    
+    /*
+     * Returns an array with the specified number of rows and columns
+     * containing the characters from str in row-major order. If str.length()
+     * is greater than rows * cols, extra characters are ignored. If
+     * str.length() is less than rows * cols, the remaining elements in the
+     * returned array contain null.
+
+     Examples:
+
+      String[][] result = fillRowMajor("Happy Halloween", 3, 4);
+      
+      result (after method call):
+      "H"     "a"     "p"     "p"
+      "y"     " "     "H"     "a"
+      "l"     "l"     "o"     "w"
+      
+      
+      String[][] result = fillRowMajor("hello", 3, 4);
+      
+      result (after method call):
+      "h"     "e"     "l"     "l"
+      "o"     "nul"     "nul"     null
+      null    null    null    null
+
+     */
+  
+    public static String[][] fillRowMajor(String str, int rows, int cols)
+    {
+        return null; 
+    }
+}    
