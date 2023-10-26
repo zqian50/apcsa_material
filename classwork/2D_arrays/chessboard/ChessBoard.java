2808d6f8590b23d5539f3bd4fd5e1ccb7646445f
zqian50 <144475267+zqian50@users.noreply.github.com>
2023-10-26 10:11:09 -0400

    Update ChessBoard.java

--- classwork/2D_arrays/chessboard/ChessBoard.java
+++ classwork/2D_arrays/chessboard/ChessBoard.java
@@ -3,6 +3,18 @@ public class ChessBoard
     public static void main(String[] args)
     {
         //Create an 8x8 2D String array called chess.
+        String[][] board = new String[8][8];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                board[i][j] = "-";
+            }
+        }
+        for(int i = 1; i < 7; i += 5){
+            for(int j = 0; j < board[0].length; j++){
+                board[i][j] = "pawn";
+            }
+        }
+        
         
         
         //Use this method to print the chess board onto the console
