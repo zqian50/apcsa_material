2f9670a9e372e0a21b7974cb117cfdb980b4b63e
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-11-01 23:48:03 -0400

    Create Extremes.java

---
+++ classwork/wrapper_classes/Extremes.java
@@ -0,0 +1,33 @@
+public class Extremes
+{
+    Integer min;
+    Integer max;
+    
+    // Constructor
+    public Extremes()
+    {
+        //Set min and max values 
+    }
+    
+    // Returns the difference
+    // max - number
+    public Integer maxDiff(Integer number)
+    {
+        
+    }
+
+    // Returns the difference
+    // min - number
+    public Integer minDiff(Integer number)
+    {
+        
+    }
+    
+    // Returns a String representation
+    // in the form
+    // [min, max]
+    public String toString()
+    {
+        return "[" + min + ", " + max + "]";   
+    }
+}
