cd80d98125a52cc2098d633d25307d7230f039af
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-11-02 22:36:30 -0400

    Add files via upload

---
+++ classwork/ArrayList/WordPair/Driver.java
@@ -0,0 +1,18 @@
+ public class Driver{
+     public static void main(String[] args)
+     {
+     	 // Array of strings
+         String[] words = {"Hi", "there", "Tyler", "Sam", "Hi", "there"};
+         
+         // Create an ArrayList with words pairs
+         // WordPairsList list = new WordPairsList(words);
+         
+         // Print the list
+         // System.out.println(list);
+         
+         // Print number of matched pairs
+         // System.out.println("The number of matched pairs is: " + list.numMatches());
+         
+         // ADD MORE TEST CASES
+     }
+ }
---
+++ classwork/ArrayList/WordPair/WordPairsList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class WordPairList
+{
+	/** The list of word pairs, initialized by the constructor. */
+	private ArrayList<WordPair> allPairs;
+	
+	
+	/** Constructs a WordPairList object as described in part (a).
+	* Precondition: words.length >= 2
+	*/
+	public WordPairList(String[] words)
+	{ /* to be implemented part (a) */ }
+	
+	
+	/** Returns the number of matches as described in part (b).
+	*/
+	public int numMatches()
+	{ /* to be implemented part (b) */ }
+}
