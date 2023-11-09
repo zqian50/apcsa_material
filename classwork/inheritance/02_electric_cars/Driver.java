aeb15e1e444a77a6b58ca216b0d3d373eb54eba3
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-11-09 00:19:44 -0500

    Create Driver.java

---
+++ classwork/inheritance/02_electric_cars/Driver.java
@@ -0,0 +1,25 @@
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        // Create a Car object
+        
+        // Print out the model
+        
+        // Print out the fuelLevel
+        
+        // Print how many miles are left with 23 mpg
+        
+        // Print the object
+        
+        // Create an ElectricCar object
+        
+        // Print out the model
+        
+        // Print out the fuelLevel
+        
+        // Print how many miles are left with 400 miles per charge
+        
+        // Print the object
+    }
+}
