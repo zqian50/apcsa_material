c22ca39b5cd87d940e3161baa39cc30c47a0c1f5
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-11-09 00:19:03 -0500

    Create ElectricCar.java

---
+++ classwork/inheritance/02_electric_cars/ElectricCar.java
@@ -0,0 +1,24 @@
+public class ElectricCar extends Car {
+
+    // Complete the constructor
+    // Note we're reinterpreting "fuelLevel" as "batteryLevel"
+    public ElectricCar(String model, int batteryLevel){
+       
+    }
+
+    // Override getFuelLevel
+    // Reinterprets fuelLevel() as a percentage
+    // Remember super.getFuelLevel will return fuelLevel!
+    
+
+    // Override milesLeft
+    // Takes one parameter - the total number of miles an ElectricCar
+    // gets on a full charge.
+    // Computes miles left by interpreting fuelLevel as the
+    // battery percentage left in the car
+    
+    
+    // Override toString
+    // Should print: model electric car
+    
+}
