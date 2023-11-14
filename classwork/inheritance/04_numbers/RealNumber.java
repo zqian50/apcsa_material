6f28dbd86923018091cefe199d902e5fcb101224
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-11-13 21:38:10 -0500

    Create RealNumber.java

---
+++ classwork/inheritance/04_numbers/RealNumber.java
@@ -0,0 +1,62 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
+  }
+  
+  /**
+  *@return 0 when other is the same as this,
+    a positive value when this is greater than other, 
+    or a negative value when this is less than other
+  */
+  public int compareTo(RealNumber other){
+    //complete this method
+    return 0;
+  }
+  
+  /**
+  *@return the sum of this and the other
+  *@param other - ANY RealNumber, including a RationalNumber or other subclasses of RealNumber (that aren't written yet)
+  */
+  public double add(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+  /**
+  *@return the product of this and the other
+  *@param other - ANY RealNumber, including a RationalNumber or other subclasses of RealNumber (that aren't written yet)
+  */
+  public double multiply(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+  /**
+  *@return the this divided by the other, use the / operator and don't worry about errors.
+  */
+  public double divide(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+  /**
+  *@return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+
+
+}
