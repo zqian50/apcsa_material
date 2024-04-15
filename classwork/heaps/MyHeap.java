361480df0ad19fa90b6ada4b1ab80a5cc0175d68
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-04-15 10:10:04 -0400

    Update MyHeap.java

--- classwork/heaps/MyHeap.java
+++ classwork/heaps/MyHeap.java
@@ -1,13 +1,15 @@
-public class MyHeap {
-        private int[] Heap;
+public class Heap{
+        private int[] heap;
         private int size;
         private int maxsize;
+        private boolean isMaxHeap;
 
-        public MyHeap(int numElements) {
-            maxsize = numElements;
-            size = 0;
-            Heap = new int[maxsize + 1];
-        }
+        public MyHeap(int numElements, boolean isMax) {
+             maxsize = numElements;
+             size = 0;
+             heap = new int[maxsize];
+             isMaxHeap = isMax;
+          }
 
         private int parent(int pos) {
             // RETURN POSTION PARENT
@@ -18,43 +20,32 @@ public class MyHeap {
             // RETURN POSITION LEFTCHILD
             return 0 ;
         }
-
+        
         private int rightChild(int pos) {
             // RETURN POSITION LEFTCHILD
             return 0 ;
         }
-
-
+        
         private void swap(int idx1, int idx2) {
-            // YOUR CODE HERE
+                // YOUR CODE HERE
         }
+        
 
         private void downHeapify(int pos) {
-            if (pos >= (size / 2) && pos <= size)
-                return;
-
-            if (Heap[pos] < Heap[leftChild(pos)] || Heap[pos] < Heap[rightChild(pos)]) {
-
                 // YOUR CODE HERE
-            }
         }
         
         private void heapifyUp(int pos) {
-            int temp = Heap[pos];
-            while(pos>0 && temp > Heap[parent(pos)]){
                 // YOUR CODE HERE
-            }
-            Heap[pos] = temp;
         }
 
-
         public void insert(int element) {
-            // YOUR CODE HERE
+                // YOUR CODE HERE
 
         }
 
         public void print() {
-            // YOUR CODE HERE
+                // YOUR CODE HERE
         }
 
         public int deleteRoot() {
